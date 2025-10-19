492ec3436cec71ff18a3a9e76c242e77b01dc9f6
Next Developer <information.instaforex@gmail.com>
2025-10-19 22:43:03 +0600

    deploy

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const dbConnect = require('./utils/db')
 dbConnect()
 
  app.use(cors({
-    origin:process.env.mode === 'pro' ? [process.env.CLIENT_CUSTOMER_PRO_URL, process.env.CLIENT_ADMIN_PRO_URL]:['http://localhost:5173','http://localhost:5174'],
+    origin:process.env.mode === 'pro' ? [process.env.CLIENT_URL, process.env.CLIENT_ADMIN_PRO_URL]:['http://localhost:5173','http://localhost:5174'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
@@ -22,13 +22,13 @@ dbConnect()
 
 const io = socket(server, {
     cors: {
-        origin: process.env.mode === 'pro' ? [process.env.CLIENT_CUSTOMER_PRO_URL, process.env.CLIENT_ADMIN_PRO_URL]:['http://localhost:5173','http://localhost:5174'],
+        origin: process.env.mode === 'pro' ? [process.env.CLIENT_URL, process.env.CLIENT_ADMIN_PRO_URL]:['http://localhost:5173','http://localhost:5174'],
         credentials: true
     }
 }) 
 
 /*     app.use(cors({
-    origin:[process.env.CLIENT_CUSTOMER_PRO_URL, process.env.CLIENT_ADMIN_PRO_URL],
+    origin:[process.env.CLIENT_URL, process.env.CLIENT_ADMIN_PRO_URL],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
@@ -36,7 +36,7 @@ const io = socket(server, {
 
 const io = socket(server, {
     cors: {
-        origin: [process.env.CLIENT_CUSTOMER_PRO_URL, process.env.CLIENT_ADMIN_PRO_URL],
+        origin: [process.env.CLIENT_URL, process.env.CLIENT_ADMIN_PRO_URL],
         credentials: true
     }
 }) */
