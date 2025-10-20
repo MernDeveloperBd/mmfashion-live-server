ba57d6cc9c4d6ea7673239c138e4aac5c1542cc4
Next Developer <information.instaforex@gmail.com>
2025-10-20 13:46:47 +0600

    deploy

--- utils/mailer.js
+++ utils/mailer.js
@@ -3,7 +3,7 @@ const nodemailer = require('nodemailer');
 const FROM_NAME = process.env.NODEMAILER_USER || 'MM Fashion World';
 const FROM_EMAIL = process.env.NODEMAILER_EMAIL;
 
-const transporter = nodemailer.createTransporter({
+const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 587,
   secure: false,
@@ -15,7 +15,6 @@ const transporter = nodemailer.createTransporter({
   maxConnections: 5,
   maxMessages: 100
 });
-
 transporter.verify().then(() => console.log('SMTP OK')).catch(console.error);
 
 async function sendMail({ to, subject, html, text }) {
